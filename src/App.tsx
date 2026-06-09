7b9e09e17e0ef55ea7f480cf6dbbed963c47207d
jagrit0711 <jagrit@zuup.dev>
2026-06-09 11:15:07 +0530

    chore: purge dead legacy routes

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,10 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 import Index from "./pages/Index";
-import Team from "./pages/Team";
-import TermsAndConditions from "./pages/TermsAndConditions";
-import FamilyLoveChecker from "./pages/FamilyLoveChecker";
-import Jobs from "./pages/Jobs";
-import JobRecommendations from "./pages/JobRecommendations";
 import OurStory from "./pages/OurStory";
 import About from "./pages/About";
 import Empower from "./pages/Empower";
-import JagritSachdev from "./pages/JagritSachdev";
-import Hackathon from "./pages/Hackathon";
 import Join from "./pages/Join";
-import Gallery from "./pages/Gallery";
 import Schools from "./pages/Schools";
 import Saas from "./pages/Saas";
 import Events from "./pages/Events";
@@ -69,17 +61,9 @@ const App = () => (
          <ViewTracker />
         <Routes>
           <Route path="/" element={<Index />} />
-          <Route path="/team" element={<Team />} />
-          <Route path="/terms" element={<TermsAndConditions />} />
-          <Route path="/family-check" element={<FamilyLoveChecker />} />
-          <Route path="/jobs" element={<Jobs />} />
-          <Route path="/job-recommendations" element={<JobRecommendations />} />
           <Route path="/our-story" element={<OurStory />} />
           <Route path="/about" element={<About />} />
           <Route path="/empower" element={<Empower />} />
-          <Route path="/jagrit-sachdev" element={<JagritSachdev />} />
-          <Route path="/hackathon" element={<Hackathon />} />
-          <Route path="/gallery" element={<Gallery />} />
           <Route path="/schools" element={<Schools />} />
           <Route path="/join" element={<Join />} />
           <Route path="/saas" element={<Saas />} />
